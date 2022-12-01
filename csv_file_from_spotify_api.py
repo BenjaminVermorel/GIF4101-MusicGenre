825697315f493aa7d1adfcd4ebd3a7dc6c378805
t2vtr <titouan.vitrai@gmail.com>
2022-12-01 18:23:20 -0500

    Ajout du script pour pull les data de spotify

---
+++ csv_file_from_spotify_api.py
@@ -0,0 +1,42 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import csv
+
+"""
+pip install spotipy
+export SPOTIPY_CLIENT_ID=4355e1e5d95c40099d5759362c8db140
+export SPOTIPY_CLIENT_SECRET=5788e8b63c8f4f1a83a4b8cb05381229
+export SPOTIPY_REDIRECT_URI=https://localhost:8888/callback
+"""
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+BLUES_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+CLASSICAL_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+JAZZ_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+METAL_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+POP_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+DISCO_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+ROCK_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+COUNTRY_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+HIPHOP_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+REGGAE_ID = '1h0CEZCm6IbFTbxThn6Xcs'
+
+LABEL = ["blues", "classical", "rock", "metal", "pop", "disco", "jazz", "country", "hiphop", "reggae"]
+ALL_ID = [BLUES_ID, CLASSICAL_ID, ROCK_ID, METAL_ID, POP_ID, DISCO_ID, JAZZ_ID, COUNTRY_ID, HIPHOP_ID, REGGAE_ID]
+INTERESSANT_KEYS = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms']
+
+with open('data_spotify.csv', 'w', newline='') as csvfile:
+    fieldnames = INTERESSANT_KEYS.copy()
+    fieldnames.append("genre")
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for playlist_id, label in zip(ALL_ID[:1], LABEL[:1]):
+        results = sp.playlist(playlist_id)
+        uris = [x["track"]["uri"] for x in results["tracks"]["items"][:100]]
+        features = sp.audio_features(uris)
+        for feature in features:
+            dico = {x : feature[x] for x in INTERESSANT_KEYS}
+            dico["genre"] = label
+            writer.writerow(dico)
